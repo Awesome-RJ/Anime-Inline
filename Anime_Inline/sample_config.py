827483af491b6273ebab2b9aefee199134c72607
「 Rajkumar™ 」 <71130660+Awesome-RJ@users.noreply.github.com>
2021-09-09 12:26:12 +0530

    | 𝐘ū𝐤𝐢 • 𝐁𝐥𝐚𝐜𝐤 𝐊𝐧𝐢𝐠𝐡𝐭𝐬 𝐔𝐧𝐢𝐨𝐧 |

---
+++ Anime_Inline/sample_config.py
@@ -0,0 +1,9 @@
+class Config(object):
+    API_ID = ""
+    API_HASH = ""
+    BOT_TOKEN = ""
+    LOG_CHANNEL = ""
+    BOT_USERNAME = ""
+    BOT_NAME = ""
+    INLINE_PIC = ""
+    INLINE_PIC_1 = ""
