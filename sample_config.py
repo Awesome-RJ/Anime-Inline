356bc79e38da265eff3eb1ec2df3e99d789a59b9
「 Rajkumar™ 」 <71130660+Awesome-RJ@users.noreply.github.com>
2021-09-09 12:26:22 +0530

    Delete sample_config.py

--- sample_config.py
+++
@@ -1,9 +0,0 @@
-class Config(object):
-    API_ID = ""
-    API_HASH = ""
-    BOT_TOKEN = ""
-    LOG_CHANNEL = ""
-    BOT_USERNAME = ""
-    BOT_NAME = ""
-    INLINE_PIC = ""
-    INLINE_PIC_1 = ""
